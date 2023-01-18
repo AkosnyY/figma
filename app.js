da4ecbb4fe44a28e4115a1cf14189a5b123dd8fb
akosy <develllopeurweb@gmail.com>
2023-01-18 11:32:19 +0100

    Modifications du fichiers total html , scss

---
+++ app.js
@@ -0,0 +1,96 @@
+
+const contactForm = document.getElementById('contactForm');
+
+contactForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const firstNameInput = document.getElementById("firstName");
+    const emailInput = document.getElementById("email");
+    const lastNameInput = document.getElementById("lastName");
+    const phoneInput = document.getElementById("phone");
+    const messageInput = document.getElementById("message");
+
+    const formData = {
+        firstName: firstNameInput.value,
+        lastName: lastNameInput.value,
+        email: emailInput.value,
+        phone: phoneInput.value,
+        message: messageInput.value,
+
+    }
+
+    const errors = {
+        firstName: false,
+        lastName: false,
+        email: false,
+        phone: false,
+        message: false
+
+    }
+
+    const firstNameError = document.getElementById('firstNameError');
+    const emailError = document.getElementById('emailError');
+    const lastNameError = document.getElementById('lastNameError');
+    const phoneError = document.getElementById('phoneError');
+    const messageError = document.getElementById('messageError');
+
+    firstNameError.style.display = 'none';
+    emailError.style.display = 'none';
+    lastNameError.style.display = 'none';
+    phoneError.style.display = 'none';
+    messageError.style.display = 'none';
+
+
+
+    if (!formData.firstName || !formData.lastName || !formData.email || !formData.phone || !formData.message) {
+        const emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm;
+        const nameRegex = /^[a-zA-Z ]+$/
+        const phoneRegex = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/
+
+
+
+        if (!formData.firstName || !nameRegex.test(formData.firstName)) {
+            errors.firstName = true;
+            firstNameError.style.display = 'block';
+        }
+        if (!formData.lastName || !nameRegex.test(formData.lastName)) {
+            errors.lastName = true;
+            lastNameError.style.display = 'block';
+        }
+        if (!formData.email || !emailRegex.test(formData.email)) {
+            errors.email = true;
+            emailError.style.display = 'block';
+        }
+        if (!formData.phone || !phoneRegex.test(formData.phone)) {
+            errors.phone = true;
+            phoneError.style.display = 'block';
+        }
+        if (!formData.message || formData.message.length < 4) {
+            errors.message = true;
+            messageError.style.display = 'block';
+        }
+    }
+
+
+    if (!Object.values(errors).includes(true)) {
+        console.log(formData)
+    }
+
+
+
+
+})
+
+
+if (!error) {
+    axios.post(' http://212.83.176.255:3030/contact', userData)
+
+        .then((response) => {
+            console.log(response.data);
+        })
+
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
