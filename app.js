960eaa1ca70a523a2f3863db9d1b63708bc12393
akosy <develllopeurweb@gmail.com>
2023-03-09 11:51:42 +0100

    Ajout d'un carousel en js

--- app.js
+++ app.js
@@ -1,4 +1,53 @@
 
+class Carrousel {
+    constructor(images, htmlId) {
+        this.images = images;
+        this.htmlId = htmlId;
+        this.carrouselImageContainer = document.getElementById(htmlId);
+        this.indexActuel = 0;
+        this.carouselElement = document.createElement('img'); 
+        this.display();
+    }
+    display = () => {
+        this.carouselElement.setAttribute('alt', "Alternative text");
+        this.carouselElement.setAttribute('src', this.images[this.indexActuel]);
+
+        this.carrouselImageContainer.appendChild(this.carouselElement);
+    }
+    suivant = () => {
+        if (!this.images[this.indexActuel + 1]) {
+            this.indexActuel = 0;
+        } else {
+            this.indexActuel += 1;
+        }
+        this.display()
+    }
+    precedent = () => {
+        
+        if (!this.images[this.indexActuel - 1]) {
+            this.indexActuel = this.images.length - 1;
+        } else {
+            this.indexActuel -= 1;
+        }
+        this.display()
+    }
+}
+
+const imagesArray = ["https://img.freepik.com/photos-gratuite/vue-dessus-du-sable-rencontre-eau-mer_158595-6255.jpg?w=1380&t=st=1678359058~exp=1678359658~hmac=57e2d84d6b6193a6d330a8d652229dc1737a7f023d765b3bc3d03a8e9070665a", "https://img.freepik.com/photos-gratuite/3d-rendu-paysage-tropical-palmiers-mer-bleue_1048-6386.jpg?w=1380&t=st=1678358918~exp=1678359518~hmac=d84aa977db4b8fe197a62e3bb539c1e26dc00a10be611a64c67d59e5838fa8df", "https://img.freepik.com/photos-gratuite/fond-eau-mer_64049-164.jpg?w=1380&t=st=1678358958~exp=1678359558~hmac=e3adda73ae251b907d8de4fa8917807ef19d87f50959c4f384850ea1c760bae2"]
+const carrousel1 = new Carrousel(imagesArray, "carouselContent")
+
+const previousArrow = document.getElementById('arrowLeft');
+const nextArrow = document.getElementById('arrowRight');
+
+
+previousArrow.addEventListener('click', () => carrousel1.precedent())
+nextArrow.addEventListener('click', () => carrousel1.suivant())
+
+setInterval(() => {
+    carrousel1.suivant()
+}, 3000);
+
+
 const contactForm = document.getElementById('contactForm');
 
 contactForm.addEventListener('submit', (event) => {
@@ -82,10 +131,6 @@ contactForm.addEventListener('submit', (event) => {
                 console.error(error);
             });
     }
-
-
-
-
 
 })
 
